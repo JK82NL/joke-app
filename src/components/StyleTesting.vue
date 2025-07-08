16898a446a723d888140010becd89d5474f2ca0d
JK82NL <jk-dance@hotmail.com>
2025-07-08 16:21:13 +0200

    Implements for provided styleguide.

---
+++ src/components/StyleTesting.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>test</h1>
+  </div>
+
+  <div>
+    <h2>test</h2>
+  </div>
+
+  <div>
+    <p>test</p>
+  </div>
+
+  <div>
+    <span>test</span>
+  </div>
+</template>
+
+<style scoped></style>
